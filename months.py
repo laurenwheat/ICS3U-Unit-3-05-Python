f7721768530f20018799d11db7debec4e7b000a9
Ubuntu <ubuntu@ip-172-31-59-214.ec2.internal>
2021-05-06 19:29:32 +0000

    assigned numbers to months

---
+++ months.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Created by: Lauren Wheatley
+# Created on: May 2021
+# This program corresponds numbers with months
+
+
+def main():
+    # this function corresponds numbers with months
+
+    month = int(input("Enter a number 1-12 (Ex. May is 5) : "))
+
+    if month == 1:
+        print("Janruary")
+    elif month == 2:
+        print("February")
+    elif month == 3:
+        print("March")
+    elif month == 4:
+        print("April")
+    elif month == 5:
+        print("May")
+    elif month == 6:
+        print("June")
+    elif month == 7:
+        print("July")
+    elif month == 8:
+        print("August")
+    elif month == 9:
+        print("September")
+    elif month == 10:
+        print("October")
+    elif month == 11:
+        print("November")
+    elif month == 12:
+        print("December")
+
+
+if __name__ == "__main__":
+    main()
